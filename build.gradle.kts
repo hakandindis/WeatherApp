692b23208e2c573aa388b125ff0f03810b7b4626
Hakan Dındış <hakandindis@gmail.com>
2024-06-17 18:44:02 +0300

    Feature: Core dependencies and plugins added to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.com.google.devtools.kts) apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    alias(libs.plugins.navigation.safeargs) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.firebaseCrashlytics) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.baselineprofile) apply false
+}
+buildscript {
+    dependencies {
+        classpath(libs.navigationSafeArgs)
+        classpath(libs.gradle)
+        classpath(libs.google.services)
+        classpath(libs.firebase.crashlytics.gradle)
+    }
 }
